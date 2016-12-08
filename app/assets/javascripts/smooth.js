3b7360a2a7281d61133564bb650276e62632c548
Ollie Pegram <olliepegram@gmail.com>
2016-12-08 16:55:09 +1100

    added footer links

--- app/assets/javascripts/smooth.js
+++ app/assets/javascripts/smooth.js
@@ -1,10 +1,3 @@
-//= require jquery.validate/jquery.validate.min
-//= require spree
-//= require spree/frontend/checkout
-//= require spree/frontend/product
-//= require spree/frontend/cart
-//= require bootstrap-sprockets
-
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
